27a9ce87bc2cf815a296e6d5683e24e334d7837c
lkiversonlk <city.of.beijing@gmail.com>
2015-06-09 15:58:29 +0800

    JSON schema support
    use jsonschema to add schema check

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var validator = require("jsonschema").validate;
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
