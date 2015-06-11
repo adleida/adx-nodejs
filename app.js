964f80edd05a5bbde26777df5e7c3b2a6829a413
lkiversonlk <city.of.beijing@gmail.com>
2015-06-11 12:37:27 +0800

    CROS problem solve

--- app.js
+++ app.js
@@ -25,6 +25,12 @@ app.use(bodyParser.urlencoded({extended: false}));
 //app.use(cookieParser());
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next){
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    next();
+});
+
 app.use('/', routes);
 //app.use('/users', users);
 
