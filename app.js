bef740d2a29e0b3d28b2dc8e23fa8aaa543f9a27
lkiversonlk <city.of.beijing@gmail.com>
2015-06-16 14:30:49 +0800

    public folder setup

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.set('view engine', 'jade');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 //app.use(cookieParser());
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next){
     res.setHeader("Access-Control-Allow-Origin", "*");
