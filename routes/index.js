b8d97ed66ca5babc55b092cf16b922d835139cf6
lkiversonlk <city.of.beijing@gmail.com>
2015-06-17 11:21:12 +0800

    if request format error, return response and stop the auction

--- routes/index.js
+++ routes/index.js
@@ -24,21 +24,21 @@ router.post("/clk", function (req, res) {
 
 
         var validateResult = engine.validate("request", request);
-        if(validateResult.errors.length > 0){
+        if (validateResult.errors.length > 0) {
             winston.log("verbose", "request not valide");
             winston.log("debug", request);
             res.end(validateResult.errors.join(" "));
+        } else {
+            var config = req.app.get('config');
+            engine.bid(request, function (error, response) {
+                if (error) {
+                    res.end(JSON.stringify(error));
+                } else {
+                    res.end(JSON.stringify(response));
+                }
+            });
         }
-
-        var config = req.app.get('config');
-        engine.bid(request, function (error, response) {
-            if (error) {
-                res.end(JSON.stringify(error));
-            } else {
-                res.end(JSON.stringify(response));
-            }
-        });
-    } else {
+    }else{
         res.end("empty request");
     }
 });
