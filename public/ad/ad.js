1709dab2e799e7326c634a31eab42e9d023e93d4
lkiversonlk <city.of.beijing@gmail.com>
2015-10-12 09:21:26 +0800

    remove unused js

--- public/ad/ad.js
+++ public/ad/ad.js
@@ -84,3 +84,50 @@ h?e:h}else if(d.has(c[h],a))return"?"===h?e:h;return a},g={ME:"4.90","NT 3.11":"
 
 //from here start clientJS
 var client = new ClientJS();
+
+/**
+ * currenlty we will collect infomation including:
+ * 1.OS type (ANDROID, IOS, WIN, LINUX)
+ * 2.screen resolution
+ * 3.device type (Computer, IPAD, MOBILE PHONE)
+ * 4.geo information is collected by the manyoubao router and hard coded into the call script
+ *
+ * for ads display, we:
+ * send bid infomation to exchange to get ad info.
+ * according to the ad info, we'll construct proper iframe, in which we fill in the adm codes.
+ *
+ * need to do more in tracking ads info.
+ */
+
+
+var arg = "{\"adunit\": {        \"type\": 1,        \"floor\": 1.6,        \"cid\": \"1101201\",        \"param\": {            \"count\": 3        }     },    \"device\": {        \"os\": \"ANDROID\",        \"os_version\": \"4.4.4\",        \"resolution_w\": 720,        \"resolution_h\": 1080,        \"network_id\": 70120,        \"device_type\": 2,        \"device_id\": \"6C0590AFF652982CEB7B3F7D8214C508\",        \"device_id_enc\": 1,        \"delected_language\": \"zh_CN\",        \"brand\": \"xiaomi\",        \"model\": \"m2\"    },    \"geo\": {        \"country\": \"asd\",        \"city\": \"asdf\",        \"latitude\": 102.153585,        \"longtitude\": 10.5544    },    \"user_id\": \"bangbang2015001\",    \"is_test\": 1,    \"cur\": \"CNY\",    \"sdk\": \"bangbang\"}"
+
+jsonArg = JSON.parse(arg);
+xmlhttp = new XMLHttpRequest()
+xmlhttp.open("POST","/clk", true);
+//xmlhttp.open("POST","http://192.168.1.246:5000/clk/", true);
+xmlhttp.setRequestHeader("Content-Type", "application/json");
+xmlhttp.onreadystatechange = function(){
+    if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
+        jsonResponse = JSON.parse(xmlhttp.responseText);
+        display(jsonResponse);
+    }
+};
+
+xmlhttp.send(JSON.stringify(jsonObj));
+
+function display(jsonResponse){
+    adms = jsonResponse.adm;
+
+    var htmlPrefix = "<html><head><title></title></head><body style='padding:0px;margin:0px;'>";
+    var htmlSuffix = "<![if !IE]><script type='text/javascript'>document.close();</script><![endif]></body></html>";
+    document.write(htmlPrefix);
+
+    var adm = adms[0].data;
+    imgSrc = adm.img;
+    text = adm.text;
+    document.write("<div><img src='" + imgSrc + "'><p>" + text + "</p></div>");
+
+    document.write(htmlSuffix);
+
+}
